f4e6aa4797a94f1b7e8482264ee8e65952c37ee0
Fadida Junaedy <fadidajunaedy@gmail.com>
2025-12-25 12:52:03 +0700

    PREPARING TEST CASES FOR USER TEST SUITE

---
+++ src/tests/user.test.ts
@@ -0,0 +1,24 @@
+describe("E2E: User Lifecycle Flow", () => {
+  test("POST User should be success with valid data", async () => {
+    // return user with id
+  });
+
+  test("GET User should be success with valid id", async () => {
+    // getting with new user id above
+  });
+
+  test("PATCH User data update should be success", async () => {
+    // update either name or status
+    // get data and expect the change
+  });
+
+  test("DELETE User data should be success", async () => {
+    // delete user by id
+    // get data and expect that user not found
+  });
+
+  test("Verify User deletion should be success", async () => {
+    // delete user by id
+    // get data and expect that user not found
+  });
+});
